3cf6a7dafc15f7add4c848296ff2afb7cd5dcab9
Jacob Rozell <Jacob.rozell83@gmail.com>
2022-08-31 15:14:08 -0600

    Refactor and edit file structure

---
+++ Puzzle Buddy/Login/CreateAccount.swift
@@ -0,0 +1,60 @@
+//
+//  CreateAccount.swift
+//  Puzzle Buddy
+//
+//  Created by Jacob Rozell on 8/31/22.
+//
+
+import SwiftUI
+
+// MARK: - CreateAccount
+struct CreateAccount: View {
+    @EnvironmentObject var auth: FirebaseAuthProvider
+    @EnvironmentObject var eh: ErrorHandling
+    @State private var name: String = ""
+    @State private var login: String = ""
+    @State private var password: String = ""
+
+    @Binding var isActive: Bool
+
+    var body: some View {
+        VStack {
+            TextField("Name", text: $name)
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal, 16)
+
+            TextField("Email / Login ID", text: $login)
+                .disableAutocorrection(true)
+                .autocapitalization(.none)
+                .keyboardType(.emailAddress)
+                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal, 16)
+
+            SecureInputView("Password", text: $password)
+                .padding(.horizontal, 16)
+                .textFieldStyle(.roundedBorder)
+
+            Spacer()
+
+            Button {
+                Task {
+                    do {
+                        try await auth.createAccount(with: name, email: login, password: password)
+                        isActive = false
+                    } catch {
+                        eh.handle(title: "Error Creating Account", message: "\(error.localizedDescription)")
+                    }
+                }
+            } label: {
+                Text("Submit")
+            }
+            .disabled(name.isEmpty)
+        }
+    }
+}
+
+struct CreateAccount_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateAccount(isActive: .constant(true))
+    }
+}
--- Puzzle Buddy/Login/LoginView.swift
+++ Puzzle Buddy/Login/LoginView.swift
@@ -16,24 +16,33 @@ struct LoginView: View {
         if let user = auth.user {
             PuzzleView(user: user)
         } else {
-            NavigationView {
-                VStack {
-                    Image(systemName: "puzzlepiece.fill")
-                        .resizable()
-                        .aspectRatio(2/1, contentMode: .fit)
-                        .padding()
-                        .foregroundColor(.blue)
+            LoginWrapper()
+        }
+    }
+}
 
-                    Spacer()
+// MARK: - LoginWrapper
+private struct LoginWrapper: View {
+    @EnvironmentObject var auth: FirebaseAuthProvider
 
-                    LoginStack()
-                }
-                .padding()
-                .ignoresSafeArea(.all, edges: .horizontal)
-                .navigationBarTitleDisplayMode(.automatic)
-                .navigationBarTitle("Sign-Up/Sign-In")
-                .padding(.vertical)
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image(systemName: "puzzlepiece.fill")
+                    .resizable()
+                    .aspectRatio(2/1, contentMode: .fit)
+                    .padding()
+                    .foregroundColor(.blue)
+
+                Spacer()
+
+                LoginStack()
             }
+            .padding()
+            .ignoresSafeArea(.all, edges: .horizontal)
+            .navigationBarTitleDisplayMode(.automatic)
+            .navigationBarTitle("Sign-Up/Sign-In")
+            .padding(.vertical)
         }
     }
 }
@@ -136,51 +145,6 @@ private struct LoginStack: View {
             ForgotPasswordView()
         }
         .padding(16)
-    }
-}
-
-struct CreateAccount: View {
-    @EnvironmentObject var auth: FirebaseAuthProvider
-    @EnvironmentObject var eh: ErrorHandling
-    @State private var name: String = ""
-    @State private var login: String = ""
-    @State private var password: String = ""
-
-    @Binding var isActive: Bool
-
-    var body: some View {
-        VStack {
-            TextField("Name", text: $name)
-                .textFieldStyle(.roundedBorder)
-                .padding(.horizontal, 16)
-
-            TextField("Email / Login ID", text: $login)
-                .disableAutocorrection(true)
-                .autocapitalization(.none)
-                .keyboardType(.emailAddress)
-                .textFieldStyle(.roundedBorder)
-                .padding(.horizontal, 16)
-
-            SecureInputView("Password", text: $password)
-                .padding(.horizontal, 16)
-                .textFieldStyle(.roundedBorder)
-
-            Spacer()
-
-            Button {
-                Task {
-                    do {
-                        try await auth.createAccount(with: name, email: login, password: password)
-                        isActive = false
-                    } catch {
-                        eh.handle(title: "Error Creating Account", message: "\(error.localizedDescription)")
-                    }
-                }
-            } label: {
-                Text("Submit")
-            }
-            .disabled(name.isEmpty)
-        }
     }
 }
 
